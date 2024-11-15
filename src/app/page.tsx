211653033b3d8287e83c5904b3d457bdb6e150e3
Emiliano Acevedo <e.acevedo.felip@gmail.com>
2024-11-14 23:03:06 -0300

    fix: suspense on index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
 import SearchForm from "@/app/components/SearchForm";
 import SearchResults from "@/app/components/SearchResults";
 import Tabs from "@/app/components/Tabs";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <>
       <h1>Welcome to Cuidadex</h1>
       <p>Search and discover your favourite dogs and cats!</p>
-      <Tabs />
-      <SearchForm />
-      <SearchResults />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Tabs />
+        <SearchForm />
+        <SearchResults />
+      </Suspense>
     </>
   );
 }
