51b9de4ad69a37d97bb1ece589083d1386d3f54e
Emiliano Acevedo <e.acevedo.felip@gmail.com>
2024-11-15 13:18:58 -0300

    feat: basic main layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,14 @@ import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <>
-      <h1>Welcome to Cuidadex</h1>
-      <p>Search and discover your favourite dogs and cats!</p>
+    <main className="px-8 sm:px-12 lg:px-24 py-4">
+      <h1 className="text-2xl mb-2">Welcome to  Cuidadex 👋!</h1>
+      <p className="mb-5">Search and find the cats and dogs you love.</p>
       <Suspense fallback={<div>Loading...</div>}>
         <Tabs />
         <SearchForm />
         <SearchResults />
       </Suspense>
-    </>
+    </main>
   );
 }
